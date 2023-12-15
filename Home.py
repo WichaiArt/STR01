86604674ca0bd530d61190ddb3f51ea253bbaaa7
WICHAI <644230055@webmail.npru.ac.th>
2023-12-15 14:25:47 +0700

    commit

--- Home.py
+++ Home.py
@@ -4,3 +4,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('Wichai Thongprao')
 st.subheader('สาขาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/rung1.jfif')
+with col2:
+    st.image('./pic/iris2.jpg')
