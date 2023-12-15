d6ddb5870a0da8abd1f568c41ecf4eccc28ae6ae
WICHAI <644230055@webmail.npru.ac.th>
2023-12-15 15:17:59 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,50 +1,29 @@
 import streamlit as st
-import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
 
-# กำหนดรูปแบบ CSS
-st.markdown(
-    """
-    <style>
-        .stButton > button {
-            background-color: #4caf50; /* สีพื้นหลังของปุ่ม */
-            color: #fff; /* สีข้อความของปุ่ม */
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 16px;
-        }
-
-        .stButton > button:hover {
-            background-color: #45a049; /* สีพื้นหลังของปุ่มเมื่อชี้เข้าไป */
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# ตัวอย่างโค้ด Streamlit ที่ใช้งาน CSS
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header('Wichai Thongprao')
 st.subheader('สาขาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
 
 col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
 with col1:
-    st.image('./img/ART.jpg', width=150)
+    st.image('./img/ART.jpg')
 with col2:
-    st.image('./img/iris.jpg', width=150)
+    st.image('./img/iris.jpg')
 
 html_1 = """
-<div class="st-ba">
-    <center><h5>สถิติข้อมูลดอกไม้</h5></center>
+<div style="background-color:#FFD233;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
 
-dt = pd.read_csv('./data/iris.csv')
+import pandas as pd
+
+dt=pd.read_csv('./data/iris.csv')
 st.write(dt.head(10))
 
 dt1 = dt['petal.length'].sum()
@@ -55,44 +34,55 @@ dt4 = dt['sepal.width'].sum()
 dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
 
-button_show_chart = st.button("show bar chart")
-if button_show_chart:
+if st.button("show bar chart"):
     st.bar_chart(dx2)
-    st.write("Not show bar chart")
+    st.button("Not show bar chart")
+else :
+    st.button("Not show bar chart") 
 
 html_2 = """
-<div class="st-ba">
-    <center><h5>การทำนายคลาสดอกไม้</h5></center>
+<div style="background-color:#FFBF00;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายคลาสดอกไม้</h5></center>
 </div>
 """
 st.markdown(html_2, unsafe_allow_html=True)
+st.markdown("")   
 
-ptlen = st.slider("กรุณาเลือกข้อมูล petal.length", 0, 10)
-ptwd = st.slider("กรุณาเลือกข้อมูล petal.width", 0, 10)
+ptlen = st.slider("กรุณาเลือกข้อมูล petal.length",0,10)
+ptwd = st.slider("กรุณาเลือกข้อมูล petal.width",0,10)
 
 splen = st.number_input("กรุณาเลือกข้อมูล sepal.length")
 spwd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
 
-button_predict = st.button("ทำนายผล")
-if button_predict:
-    X = dt.drop('variety', axis=1)
-    y = dt.variety
-    Knn_model = KNeighborsClassifier(n_neighbors=3)
-    Knn_model.fit(X, y)
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
 
-    x_input = np.array([[ptlen, ptwd, splen, spwd]])
-    prediction = Knn_model.predict(x_input)
+if st.button("ทำนายผล"):
+   # ทำนาย
+   #dt = pd.read_csv("./data/iris.csv") 
 
-    if prediction[0] == "Setosa":
-        st.image("./img/im.jpg", width=150)
-        st.header("Setosa")
-    elif prediction[0] == "Versicolor":
-        st.image("./img/imm.jpg", width=150)
-        st.header("Versicolor")
-    else:
-        st.image("./img/i.jpg", width=150)
-        st.header("Verginiga")
+   X = dt.drop('variety', axis=1)
+   y = dt.variety   
 
-    st.write("ไม่ทำนายผล")
-else:
-    st.write("ไม่ทำนายผล")
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)
+
+    #ข้อมูล input สำหรับทดลองจำแนกข้อมูล
+   x_input = np.array([[ptlen, ptwd, splen, spwd]])
+    # เอา input ไปทดสอบ
+   st.write(Knn_model.predict(x_input))
+   out=Knn_model.predict(x_input)
+
+   if out[0]=="Setosa":
+      st.image("./img/im.jpg")
+      st.header("Setosa")
+   elif out[0]=="Versicolor":
+      st.image("./img/imm.jpg")
+      st.header("Versicolor")
+   else:
+      st.image("./img/immm.jpg")  
+      st.header("Verginiga")
+   st.button("ไม่ทำนายผล")
+else :
+    st.button("ไม่ทำนายผล")
+    
