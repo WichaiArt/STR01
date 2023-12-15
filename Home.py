931c53cd3a34a7c700b536c4283f3fd50b92bc92
WICHAI <644230055@webmail.npru.ac.th>
2023-12-15 15:10:32 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,44 +1,70 @@
 import streamlit as st
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
 
+# 1. การจัดหน้าเว็บ
+st.set_page_config(
+    page_title="Web App ทดสอบด้วย Python",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide"
+)
+
+# 2. การเรียงลำดับและจัดหน้า Element
+with st.sidebar:
+    st.subheader("ตัวเลือก")
+    ptlen = st.slider("ข้อมูล petal.length", 0, 10)
+    ptwd = st.slider("ข้อมูล petal.width", 0, 10)
+    splen = st.number_input("ข้อมูล sepal.length")
+    spwd = st.number_input("ข้อมูล sepal.width")
+    show_chart = st.checkbox("แสดงกราฟ")
+
+# 3. การใช้ Widget ที่เหมาะสม
+if show_chart:
+    st.bar_chart(dx2)
+
+# 4. การแยกฟังก์ชัน
+def show_statistics_chart():
+    # แสดงสถิติข้อมูลดอกไม้
+    st.markdown(html_1, unsafe_allow_html=True)
+    st.write(dt.head(10))
+    if show_chart:
+        st.bar_chart(dx2)
+
+def make_prediction():
+    # ทำนายผล
+    X = dt.drop('variety', axis=1)
+    y = dt.variety
+    Knn_model = KNeighborsClassifier(n_neighbors=3)
+    Knn_model.fit(X, y)
+    x_input = np.array([[ptlen, ptwd, splen, spwd]])
+    prediction = Knn_model.predict(x_input)
+    st.write(prediction)
+
+    # แสดงรูปภาพตามคลาสที่ทำนาย
+    if prediction[0] == "Setosa":
+        st.image("./img/im.jpg")
+        st.header("Setosa")
+    elif prediction[0] == "Versicolor":
+        st.image("./img/imm.jpg")
+        st.header("Versicolor")
+    else:
+        st.image("./img/immm.jpg")
+        st.header("Verginiga")
+
+# 5. การให้คำอธิบาย
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header('Wichai Thongprao')
 st.subheader('สาขาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
 
 col1, col2 = st.columns(2)
-#col1.write("This is column 1")
-#col2.write("This is column 2")
 with col1:
     st.image('./img/ART.jpg')
 with col2:
     st.image('./img/iris.jpg')
 
-html_1 = """
-<div style="background-color:#FFD233;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
-</div>
-"""
-st.markdown(html_1, unsafe_allow_html=True)
-st.markdown("")
-
-import pandas as pd
-
-dt=pd.read_csv('./data/iris.csv')
-st.write(dt.head(10))
-
-dt1 = dt['petal.length'].sum()
-dt2 = dt['petal.width'].sum()
-dt3 = dt['sepal.length'].sum()
-dt4 = dt['sepal.width'].sum()
-
-dx = [dt1, dt2, dt3, dt4]
-dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
-
-if st.button("show bar chart"):
-    st.bar_chart(dx2)
-    st.button("Not show bar chart")
-else :
-    st.button("Not show bar chart") 
+show_statistics_chart()
 
 html_2 = """
 <div style="background-color:#FFBF00;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
@@ -46,43 +72,6 @@ html_2 = """
 </div>
 """
 st.markdown(html_2, unsafe_allow_html=True)
-st.markdown("")   
+st.markdown("")
 
-ptlen = st.slider("กรุณาเลือกข้อมูล petal.length",0,10)
-ptwd = st.slider("กรุณาเลือกข้อมูล petal.width",0,10)
-
-splen = st.number_input("กรุณาเลือกข้อมูล sepal.length")
-spwd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
-
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
-
-if st.button("ทำนายผล"):
-   # ทำนาย
-   #dt = pd.read_csv("./data/iris.csv") 
-
-   X = dt.drop('variety', axis=1)
-   y = dt.variety   
-
-   Knn_model = KNeighborsClassifier(n_neighbors=3)
-   Knn_model.fit(X, y)
-
-    #ข้อมูล input สำหรับทดลองจำแนกข้อมูล
-   x_input = np.array([[ptlen, ptwd, splen, spwd]])
-    # เอา input ไปทดสอบ
-   st.write(Knn_model.predict(x_input))
-   out=Knn_model.predict(x_input)
-
-   if out[0]=="Setosa":
-      st.image("./img/im.jpg")
-      st.header("Setosa")
-   elif out[0]=="Versicolor":
-      st.image("./img/imm.jpg")
-      st.header("Versicolor")
-   else:
-      st.image("./img/immm.jpg")  
-      st.header("Verginiga")
-   st.button("ไม่ทำนายผล")
-else :
-    st.button("ไม่ทำนายผล")
-    
+make_prediction()
