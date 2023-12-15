e267e63885126eb71e9e9341788f598931c8622b
WICHAI <644230055@webmail.npru.ac.th>
2023-12-15 14:27:30 +0700

    comm

--- Home.py
+++ Home.py
@@ -9,6 +9,6 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/rung1.jfif')
+    st.image('./img/ART.jpg')
 with col2:
-    st.image('./pic/iris2.jpg')
+    st.image('./img/iris.jpg')
